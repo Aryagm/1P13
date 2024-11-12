53c79fc7e718cd77d33af5ba56fd67abac3b8dc9
Arya Manjaramkar <80019072+Aryagm@users.noreply.github.com>
2024-11-12 15:39:23 -0500

    Add files via upload

---
+++ passenger_and_fleet_data.py
@@ -0,0 +1,22 @@
+def passenger_data(filename):
+    file = open(filename, mode="r")
+    passenger_list = []
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+        passenger_list.append(line.strip().split(","))
+    
+    return passenger_list
+
+def fleet_data(filename):
+    file = open(filename, mode="r")
+    fleet_list = []
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+        fleet_list.append(line.strip().split(","))
+    
+    return fleet_list
+
+pdata = passenger_data("passenger_data_v2.txt")
+fdata = fleet_data("fleet_data.txt")
