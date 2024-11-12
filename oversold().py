c1c94cf49e807a2fe0345e2e4bea44df225c96d6
erjona505 <erjona.kalari@gmail.com>
2024-11-12 15:51:01 -0500

    Add files via upload

---
+++ oversold().py
@@ -0,0 +1,32 @@
+def oversold(passenger_list, fleet_list, daily_data_list):
+    """
+returns 2D list of the model of the plane and the # of economy passengers 
+with oversold seats for each flight in fleet_data.tx
+
+    """
+    oversold_business = []
+    oversold_economy = []
+
+    for data in daily_data_list:
+        #get gate, business & economy info from daily data
+        gate = data[0]
+        business_count = data[1]
+        economy_count = data[2]
+        
+        for plane in fleet_list:
+            if plane[4] == gate: #check if plane matches the gate
+                #get business & economy capacity from plane data
+                business_capacity = int(plane[1])
+                economy_capacity = int(plane[2])
+                
+                business_oversold = max(0, business_count - business_capacity)
+                economy_oversold = max(0, economy_count - economy_capacity)
+                
+                if business_oversold > 0:
+                    oversold_business.append([plane[0], business_oversold])
+                if economy_oversold > 0:
+                    oversold_economy.append([plane[0], economy_oversold])
+                
+                break 
+            
+    return oversold_business, oversold_economy
