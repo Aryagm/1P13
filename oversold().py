eea711ac0fd6238c02d8c7abb3862397121e588a
erjona505 <erjona.kalari@gmail.com>
2024-11-26 14:49:04 -0500

    Update oversold().py

--- oversold().py
+++ oversold().py
@@ -1,8 +1,10 @@
 def oversold(passenger_list, fleet_list, daily_data_list):
     """
-Returns 2D list of the model of the plane and the # of economy and bussiness passengers 
-with oversold seats for each flight.
+Calculate the number of oversold business & economy seats for each flight based on fleet and daily data. 
+Returns two 2D lists of oversold business seats & oversold economy seats containing the 
+plane model & oversold seat count.
     """
+    #initialize lists to store info for oversold seats in each class
     oversold_business = []
     oversold_economy = []
 
@@ -13,14 +15,16 @@ with oversold seats for each flight.
         economy_count = data[2]
         
         for plane in fleet_list:
-            if plane[4] == gate: #check if plane matches the gate
-                #get business & economy capacity from plane data
+            if plane[4] == gate: #check if plane matches the gate in fleet_list & daily_data_list
+                #get business & economy capacity for the plane
                 business_capacity = int(plane[1])
                 economy_capacity = int(plane[2])
-                
+
+                #calculate oversold seats for bussiness and economy
                 business_oversold = max(0, business_count - business_capacity)
                 economy_oversold = max(0, economy_count - economy_capacity)
-                
+
+                #append plane model and count to the respective list
                 if business_oversold > 0:
                     oversold_business.append([plane[0], business_oversold])
                 if economy_oversold > 0:
