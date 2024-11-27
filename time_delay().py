675d6dea5e3766f271ffbafb1ae72878a75f3ee9
Aarohan-eng <singa363@mcmaster.ca>
2024-11-26 23:44:24 -0500

    Add files via upload

---
+++ time_delay().py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 15:45:32 2024
+
+@author: aaroh
+"""
+
+def time_delay(passengerList, fleetList):
+    planelatelayover=[]
+    
+    for plane in fleetList:
+        plane_model = plane[0]
+        gate = plane[4]
+        late_layover_count = 0
+        
+        for passenger in passengerList:
+            if passenger[2] == gate and passenger [7] == "Layover" and passenger[8] == "Late":
+                late_layover_count += 1
+                
+     
+        planelatelayover.append([plane_model, late_layover_count])
+        
+    return planelatelayover
