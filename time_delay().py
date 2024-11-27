6f9f5d262987d64d17759193a28a7e9788d84da3
Aarohan-eng <singa363@mcmaster.ca>
2024-11-27 16:10:06 -0500

    Update time_delay().py

--- time_delay().py
+++ time_delay().py
@@ -1,12 +1,12 @@
-def time_delay(passenger_list, fleet_list):
+def time_delay(passengerList, fleetList):
     planelatelayover = []
     
-    for plane in fleet_list:
+    for plane in fleetList:
         plane_model = plane[0]
         gate = plane[4]
         late_layover_count = 0
         
-        for passenger in passenger_list:
+        for passenger in passengerList:
             if passenger[2] == gate and passenger [7] == "Layover" and passenger[8] == "Late":
                 late_layover_count += 1
                 
